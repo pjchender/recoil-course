513d05b38b54e1b6555bbc3fa76f5d9cd1a62d06
aaronchen <aaron.chen@onedegree.hk>
2021-06-03 14:59:05 +0800

    Data Fetching Advanced - selector of WeatherState depends on selector of UserState

--- src/examples/Async.tsx
+++ src/examples/Async.tsx
@@ -2,6 +2,7 @@ import {Container, Heading, Text} from '@chakra-ui/layout'
 import {Select} from '@chakra-ui/select'
 import {Suspense, useState} from 'react'
 import {selectorFamily, useRecoilValue} from 'recoil'
+import {getWeather} from './fakeAPI'
 
 const userState = selectorFamily({
     key: 'user',
@@ -12,8 +13,20 @@ const userState = selectorFamily({
     },
 })
 
+const weatherState = selectorFamily({
+    key: 'weather',
+    get:
+        (userId: number) =>
+        async ({get}) => {
+            const user = get(userState(userId)) // 這裡並不需要加上 await，recoil 會在取得 userState 後才接著執行這裡的 get function
+            const weather = await getWeather(user.address.city)
+            return weather
+        },
+})
+
 const UserData = ({userId}: {userId: number}) => {
     const user = useRecoilValue(userState(userId))
+    const weather = useRecoilValue(weatherState(userId))
 
     return (
         <div>
@@ -25,6 +38,11 @@ const UserData = ({userId}: {userId: number}) => {
             </Text>
             <Text>
                 <b>Phone:</b> {user.phone}
+            </Text>
+            <Text>
+                <b>
+                    Weather for {user.address.city}: {weather}°C
+                </b>
             </Text>
         </div>
     )
@@ -55,6 +73,7 @@ export const Async = () => {
                 <option value="3">User 3</option>
             </Select>
             {userId !== undefined && (
+                // Suspense 會在 UserData 中的所有 data 都 resolve 後才解開
                 <Suspense fallback={<div>Loading...</div>}>
                     <UserData userId={userId} />
                 </Suspense>
