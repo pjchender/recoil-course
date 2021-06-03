d1cba18c261bf1457027fa93b884570f3652987e
aaronchen <aaron.chen@onedegree.hk>
2021-06-03 12:58:46 +0800

    Data Fetching Basic - replace depencies of Atom with React State by 'selectorFamily'

--- src/examples/Async.tsx
+++ src/examples/Async.tsx
@@ -1,32 +1,19 @@
 import {Container, Heading, Text} from '@chakra-ui/layout'
 import {Select} from '@chakra-ui/select'
-import {Suspense} from 'react'
-import {atom, selector, useRecoilState, useRecoilValue} from 'recoil'
+import {Suspense, useState} from 'react'
+import {selectorFamily, useRecoilValue} from 'recoil'
 
-const userIdState = atom<number | undefined>({
-    key: 'userId',
-    default: undefined,
-})
-
-const userState = selector({
+const userState = selectorFamily({
     key: 'user',
-    get: async ({get}) => {
-        const userId = get(userIdState)
-        console.log({
-            userId,
-        })
-        if (userId === undefined) return
-
+    get: (userId: number) => async () => {
         const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
         const userData = await res.json()
         return userData
     },
 })
 
-const UserData = () => {
-    const user = useRecoilValue(userState)
-
-    if (!user) return null
+const UserData = ({userId}: {userId: number}) => {
+    const user = useRecoilValue(userState(userId))
 
     return (
         <div>
@@ -44,7 +31,7 @@ const UserData = () => {
 }
 
 export const Async = () => {
-    const [userId, setUserId] = useRecoilState(userIdState)
+    const [userId, setUserId] = useState<number>()
 
     return (
         <Container py={10}>
@@ -67,9 +54,11 @@ export const Async = () => {
                 <option value="2">User 2</option>
                 <option value="3">User 3</option>
             </Select>
-            <Suspense fallback={<div>Loading...</div>}>
-                <UserData />
-            </Suspense>
+            {userId !== undefined && (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <UserData userId={userId} />
+                </Suspense>
+            )}
         </Container>
     )
 }
